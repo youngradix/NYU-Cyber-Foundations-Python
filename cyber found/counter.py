062133dd9df24f508edaf42be918a70ee2a6ea9d
youngradix <robinsonacosta1@gmail.com>
2021-06-16 11:11:09 -0400

    Create counter.py

---
+++ cyber found/counter.py
@@ -0,0 +1,10 @@
+print("Please enter the number of coins: ")
+quarters = input("# of quarters: ")
+quarters = quarters * 0.25
+dimes = input("# of dimes: ")
+dimes = dimes * 0.10
+nickels = input("# of nickels: ")
+nickels = nickels * 0.05
+pennies = input("# of pennies: ")
+pennies = pennies * 0.01
+
