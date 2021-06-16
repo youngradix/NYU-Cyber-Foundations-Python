f66ac5c453bc3c6727ef6c7cdc311df13424f3ae
youngradix <robinsonacosta1@gmail.com>
2021-06-15 22:22:13 -0400

    Update evennumbers.py

--- cyber found/evennumbers.py
+++ cyber found/evennumbers.py
@@ -1,6 +1,8 @@
 numberIn = int(input("Please enter a positve integer: "))
 num = 0
 
-for i in range(0, numberIn):
+for i in range(0, numberIn-2):
     num = num + 2
     print(num)
+    if num >= numberIn-2:
+        break
