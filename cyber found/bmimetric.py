58462a844657e4141e8b0bd0317b6bd527242721
youngradix <robinsonacosta1@gmail.com>
2021-06-15 21:42:39 -0400

    Create bmimetric.py

---
+++ cyber found/bmimetric.py
@@ -0,0 +1,6 @@
+weight = float(input("Please enter weight in kilograms: "))
+height = float(input("Please enter height in meters: "))
+
+bmiCalculator = (weight)/(height**2)
+
+bmi = print(f'bmi is: {bmiCalculator}')
