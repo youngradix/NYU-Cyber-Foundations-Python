f74edd85992cc6fd7d99598db493e199990c8c5f
youngradix <robinsonacosta1@gmail.com>
2021-06-15 23:04:07 -0400

    Update bmiimperial.py

--- cyber found/bmiimperial.py
+++ cyber found/bmiimperial.py
@@ -1,3 +1,4 @@
+
 weight = float(input("Please enter weight in pounds: "))
 height = float(input("Please enter height in inches: "))
 weight = weight * 0.453592
@@ -5,14 +6,4 @@ height = height * 0.0254
 
 bmiCalculator = (weight)/(height**2)
 
-if bmiCalculator < 18.5:
-    status = "Underweight"
-elif bmiCalculator > 18.5 and < 24.9:
-    status = "Normal"
-elif bmiCalculator > 25.0 and < 29.9:
-    status = "Overweight"
-else:
-    status = "Obese"
-
-
-bmi = print(f'bmi is: {bmiCalculator}, Status is ' + status)
+bmi = print(f'bmi is: {bmiCalculator}')
