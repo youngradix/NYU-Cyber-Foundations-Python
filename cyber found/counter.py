1d92c654d32a073ac7f1e5f3fb4addc67e3339bf
youngradix <robinsonacosta1@gmail.com>
2021-06-24 17:55:01 -0400

    Update counter.py

--- cyber found/counter.py
+++ cyber found/counter.py
@@ -1,10 +1,16 @@
 print("Please enter the number of coins: ")
-quarters = input("# of quarters: ")
-quarters = quarters * 0.25
-dimes = input("# of dimes: ")
-dimes = dimes * 0.10
-nickels = input("# of nickels: ")
-nickels = nickels * 0.05
-pennies = input("# of pennies: ")
-pennies = pennies * 0.01
 
+quarters = int(input("# of quarters: "))
+dimes = int(input("# of dimes: "))
+nickels = int(input("# of nickels: "))
+pennies = int(input("# of pennies: "))
+quarters = quarters * 0.25
+
+dimes = dimes * 0.10
+nickels = nickels * 0.05
+pennies = pennies * 0.01
+total = quarters + dimes + nickels + pennies
+total = str(total)
+dollars = total[0]
+cents = total[2:4]
+print('The total is', dollars, 'dollars and', cents, 'cents.')
