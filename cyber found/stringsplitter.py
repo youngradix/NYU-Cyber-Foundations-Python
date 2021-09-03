21491c220d2315455498db21756b53108a1ef1ef
youngradix <robinsonacosta1@gmail.com>
2021-09-03 00:07:03 -0400

    Update stringsplitter.py

--- cyber found/stringsplitter.py
+++ cyber found/stringsplitter.py
@@ -1,4 +1,9 @@
-
 stdIn = input("Enter an odd length string: ")
-middleChar = (len(stdIn)-1)/2
-print("Middle char: ", middleChar)
+stringSize = len(stdIn)
+middleIndex = int((len(stdIn)-1)/2)
+middleChar = stdIn[middleIndex]
+print("Middle character: ", middleChar)
+firstHalf = stdIn[:middleIndex]
+print("First half: ", firstHalf)
+secondHalf = stdIn[middleIndex + 1:]
+print("Second half: ", secondHalf)
